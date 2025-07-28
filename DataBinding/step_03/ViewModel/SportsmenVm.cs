3d09f62b135e6c7e5da74432d43362862e28ed9b
Are0pag <154928079+Are0pag@users.noreply.github.com>
2025-07-29 00:19:38 +0300

    //

--- DataBinding/step_03/ViewModel/SportsmenVm.cs
+++ DataBinding/step_03/ViewModel/SportsmenVm.cs
@@ -7,26 +7,14 @@ namespace Demo.DataBinding.step_03
 {
     public class SportsmenVm : INotifyPropertyChanged
     {
-        protected readonly Sportsmen _sportsmen;
-
         public SportsmenVm(Sportsmen sportsmen) {
             _sportsmen = sportsmen;
-            _sportsmen.PropertyChanged += (_, e) => OnPropertyChanged(e.PropertyName);
-            // Подписываемся на изменения коллекции
-            _sportsmen.Achievements.CollectionChanged += (_, _) => {
-                                                             OnPropertyChanged(nameof(Achievements));
-                                                         };
+            SubscribeOnModelEvent();
+            SubscribeOnCollectionChanged();
         }
 
-        public string SportsmenId {
-            get => _sportsmen.SportsmenID;
-            set {
-                if (_sportsmen.SportsmenID != value) {
-                    _sportsmen.SportsmenID = value;
-                    OnPropertyChanged();
-                }
-            }
-        }
+
+        #region CollectionVm
 
         public ObservableCollection<Achievements> Achievements {
             get => _sportsmen.Achievements;
@@ -42,15 +30,43 @@ namespace Demo.DataBinding.step_03
                 }
             }
         }
-        
+
+        private void SubscribeOnCollectionChanged() {
+            _sportsmen.Achievements.CollectionChanged += (_, _) => {
+                                                             OnPropertyChanged(nameof(Achievements));
+                                                         };
+        }
+
         private void OnAchievementsCollectionChanged(object? sender, NotifyCollectionChangedEventArgs e) {
             OnPropertyChanged(nameof(Achievements));
         }
-        
+
+        #endregion
+
+        #region PropertyVm
+
+        protected readonly Sportsmen _sportsmen;
+        public string SportsmenId {
+            get => _sportsmen.SportsmenID;
+            set {
+                if (_sportsmen.SportsmenID != value) {
+                    _sportsmen.SportsmenID = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+        private void SubscribeOnModelEvent() {
+            _sportsmen.PropertyChanged += (_, e) => OnPropertyChanged(e.PropertyName);
+        }
+
         public event PropertyChangedEventHandler? PropertyChanged;
 
         protected virtual void OnPropertyChanged([CallerMemberName] string? propertyName = null) {
             PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
         }
+
+        #endregion
+        
     }
 }
