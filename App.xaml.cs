30cd75365740df34f7ad3daeaf5ccca97707952b
Are0pag <154928079+Are0pag@users.noreply.github.com>
2025-08-25 20:42:20 +0300

    ew

--- App.xaml.cs
+++ App.xaml.cs
@@ -1,18 +1,13 @@
-﻿using Demo.DataBinding.step_03;
-using System.Configuration;
-using System.Data;
-using System.Windows;
+﻿using System.Windows;
+using Demo.DataBinding.step_05.View;
 
 namespace Demo
 {
-    /// <summary>
-    /// Interaction logic for App.xaml
-    /// </summary>
     public partial class App : Application
     {
         protected override void OnStartup(StartupEventArgs e) {
             base.OnStartup(e);
-            new MyWindow(new SportsmenVm(new Sportsmen { SportsmenID = "tuy 20.4445" })).Show();
+            new GridOfCompositeObjectsWindow(new FullGridWindowVm()).Show();
         }
     }
 
